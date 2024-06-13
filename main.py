233bc1585fbb8de3bea617e953878590cdae71d2
Jim Barlow <jim@jimbarlow.com>
2024-06-13 13:37:59 -0400

    redirected to form page

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return redirect("https://harthumane.org")
+    return redirect("https://harthumane.org/poker-run")
+    # return render_template('home.html')
 
 if __name__ == "__main__":
   app.run()
